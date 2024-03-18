116bd2e439d593e80968a6624efe8b764746052d
pabu2 <pabu@cloudhight.com>
2024-03-18 20:21:42 +0000

    Adding Internet gateway

--- main.tf
+++ main.tf
@@ -5,3 +5,25 @@ provider "aws" {
 resource "aws_vpc" "sol_vpc" {
   cidr_block = "10.0.0.0/16"
 }
+# Internet Gateway
+resource "aws_internet_gateway" "pet_gw" {
+  vpc_id = aws_vpc.sol_vpc.id
+
+  tags = {
+    Name = "Pet_IGW"
+  }
+}
+
+# Route Table for IGW
+resource "aws_route_table" "pet_rt" {
+  vpc_id = aws_vpc.sol_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.pet_gw.id
+  }
+
+  tags = {
+    Name = "pet_rt"
+  }
+}
